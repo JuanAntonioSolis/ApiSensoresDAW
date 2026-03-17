cb162c72630d7bf6b50d243e81e803e0df17cf4f
JuanAntonioSolis <jsolcor457@g.educaand.es>
2026-03-17 21:15:41 +0100

    17MarzoMQTTPublisher

--- src/main/java/com/jaroso/apisensoresdaw/controllers/SensorController.java
+++ src/main/java/com/jaroso/apisensoresdaw/controllers/SensorController.java
@@ -6,13 +6,16 @@ import com.jaroso.apisensoresdaw.dtos.SensorUpdateDto;
 import com.jaroso.apisensoresdaw.entities.Sensor;
 import com.jaroso.apisensoresdaw.mappers.SensorMapper;
 import com.jaroso.apisensoresdaw.repositories.SensorRepository;
+import com.jaroso.apisensoresdaw.services.MqttPublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @RestController
@@ -23,6 +26,11 @@ public class SensorController {
     private SensorRepository sensorRepository;
     @Autowired
     private SensorMapper sensorMapper;
+    @Autowired
+    private MqttPublisher mqttPublisher;
+
+    Logger logger = Logger.getLogger(LectureController.class.getName());
+
 
     @GetMapping
     public ResponseEntity<List<SensorDto>> findAll() {
@@ -55,9 +63,16 @@ public class SensorController {
 
     @PutMapping("/{id}")
     public ResponseEntity<SensorDto> updateSensor(@PathVariable Long id, @RequestBody SensorUpdateDto sensorUpdateDto) {
+        logger.info("Actualizando sensor: " + sensorUpdateDto);
         Optional<Sensor> sensor = sensorRepository.findById(id);
+
         if (sensor.isPresent()) {
             sensor.get().setState(sensorUpdateDto.state());
+
+            // publica un mensaje MQTT al topic del actuador (ej: actuadores/1/comando con payload ON o OFF)
+            String payload = String.format("{\"state\": \"%s\"}", sensorUpdateDto.state());
+            mqttPublisher.publish("test/sensor/bomba", payload);
+
             return ResponseEntity.ok(sensorMapper.sensorToDto(sensorRepository.save(sensor.get())));
         } else {
             return ResponseEntity.notFound().build();
---
+++ src/main/java/com/jaroso/apisensoresdaw/services/MqttPublisher.java
@@ -0,0 +1,34 @@
+package com.jaroso.apisensoresdaw.services;
+
+import com.hivemq.client.mqtt.mqtt3.Mqtt3BlockingClient;
+import com.hivemq.client.mqtt.mqtt3.Mqtt3Client;
+import com.jaroso.apisensoresdaw.controllers.LectureController;
+import com.jaroso.apisensoresdaw.entities.Lecture;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class MqttPublisher {
+    private final Mqtt3BlockingClient client;
+    Logger logger = Logger.getLogger(LectureController.class.getName());
+
+    public MqttPublisher(@Value("${mqtt.host:localhost}") String host,
+                         @Value("${mqtt.port:1883}") int port) {
+        client = Mqtt3Client.builder()
+                .identifier("springClient")
+                .serverHost(host)
+                .serverPort(port)
+                .buildBlocking();
+        client.connect();
+    }
+
+    public void publish(String topic, String payload) {
+        logger.info("Publicando en " + topic + ": " + payload);
+        client.publishWith()
+                .topic(topic)
+                .payload(payload.getBytes())
+                .send();
+    }
+}
